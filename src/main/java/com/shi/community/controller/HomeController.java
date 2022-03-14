05119982e95c86e88b21403fdcbcf7492eac6e1d
shiyi <318761557@qq.com>
2022-03-14 20:15:55 +0800

    error处理

--- src/main/java/com/shi/community/controller/HomeController.java
+++ src/main/java/com/shi/community/controller/HomeController.java
@@ -8,6 +8,7 @@ import com.shi.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -43,6 +44,10 @@ public class HomeController {
         }
         model.addAttribute("discussPosts",discussPosts);
         return "index";
+    }
+    @GetMapping("/error")
+    public String error(){
+        return "/error/500";
     }
 
 }
